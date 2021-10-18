15cf5da1b0cf86ac86f5238ccf457d45e660ceb7
Luca Actis Grosso <luca.actisgrosso@generali.com>
2021-10-18 17:45:21 +0200

    added tox and github actions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 package_root = os.path.abspath(os.path.dirname(__file__))
 
 version = {}
-with open(os.path.join(package_root, "src/lit_saint/version.py")) as fp:
+with open(os.path.join(package_root, "src/lit_tracking/version.py")) as fp:
     exec(fp.read(), version)
 version = version["__version__"]
 
