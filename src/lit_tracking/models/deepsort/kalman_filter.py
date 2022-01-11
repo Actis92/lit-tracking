6e7b9ff38c6b8f9a22a9a692d26cd25c2025cb1e
Luca Actis Grosso <luca.actisgrosso@generali.com>
2022-01-11 16:34:18 +0100

    star work on kalman filter

---
+++ src/lit_tracking/models/deepsort/kalman_filter.py
@@ -0,0 +1,18 @@
+class DeepSortKalmanFilter:
+    def __init__(self):
+        self.kf = None
+
+    def initiate(self, measurement):
+        """Create track from unassociated measurement.
+        Parameters
+        ----------
+        measurement : ndarray
+            Bounding box coordinates (x, y, a, h) with center position (x, y),
+            aspect ratio a, and height h.
+        Returns
+        -------
+        (ndarray, ndarray)
+            Returns the mean vector (8 dimensional) and covariance matrix (8x8
+            dimensional) of the new track. Unobserved velocities are initialized
+            to 0 mean.
+        """
