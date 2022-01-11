700e23143eb09f896d259aa19d8828ee49288eaf
Luca Actis Grosso <luca.actisgrosso@generali.com>
2022-01-11 13:21:44 +0100

    update example using flash

--- src/lit_tracking/utils/flash_example.py
+++ src/lit_tracking/utils/flash_example.py
@@ -1,27 +1,37 @@
+from dataclasses import dataclass
+
 import flash
+from flash import InputTransform
 from flash.image import ObjectDetectionData, ObjectDetector
 from icevision.tfms import A
 
 from flash.core.integrations.icevision.transforms import IceVisionTransformAdapter
 
+@dataclass
+class ResizeTransform(InputTransform):
+
+    image_size: int = 128
+
+    def per_sample_transform(self):
+        return IceVisionTransformAdapter(
+            [*A.aug_tfms(size=1024, presize=1920), A.Normalize()]
+        )
+
+
 
 if __name__ == "__main__":
-    train_transform = {
-        "pre_tensor_transform": IceVisionTransformAdapter(
-            [*A.aug_tfms(size=(1024, 896), presize=(1920, 1080)), A.Normalize()]
-        )
-    }
     datamodule = ObjectDetectionData.from_coco(
         train_folder="../../../tests/data/coco/train/data",
         train_ann_file="../../../tests/data/coco/train/labels.json",
         val_split=0.5,
-        image_size=(1024, 896),
-        train_transform=train_transform
+        batch_size=1,
+        train_transform=ResizeTransform,
+        val_transform=ResizeTransform
     )
 
     # 2. Build the task
     model = ObjectDetector(head="efficientdet", backbone="d0", num_classes=datamodule.num_classes,
-                           image_size=(1024, 896))
+                           image_size=1024)
 
     # 3. Create the trainer and finetune the model
     trainer = flash.Trainer(max_epochs=1)
